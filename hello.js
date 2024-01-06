3ecd0c2fe0e108cf192be11a55459433a034e898
tjay1760 <mcjthiongo@gmail.com>
2024-01-06 22:33:23 +0300

    module import

---
+++ hello.js
@@ -0,0 +1,4 @@
+const sayHello = (name) =>{
+    console.log(`hello ${name}`);
+}
+module.exports = sayHello
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+const sayHello = require('./hello')
 console.log("The dir name", __dirname);
 console.log("The  file name",__filename);
 
 global.myVAr = "New var"
 console.log(globalThis)
+sayHello("VAlentine")
+sayHello("shayne")

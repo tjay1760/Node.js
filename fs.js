28e993b0ed396e6a4d17bcbd652161e11b0dcd7c
tjay1760 <mcjthiongo@gmail.com>
2024-01-07 02:02:41 +0300

    Asynchronous read operations

--- fs.js
+++ fs.js
@@ -1,19 +1,28 @@
 const fs = require ('fs');
 const data = '\n My first node redone again'
 
-fs.mkdir('./testfolder', 
-(err) => {
-    err ? console.log(err.code): console.log("Folder created succesfully") 
+// fs.mkdir('./testfolder', 
+// (err) => {
+//     err ? console.log(err.code): console.log("Folder created succesfully") 
+
+// }
+// )
+// fs.writeFile('./testfolder/test.txt', data,{flag:'a'},
+// (err)=>{
+//     err? console.log(err.code): console.log("Write succesfull")
+// }
+// )
+// fs.readFile('./testfolder/test.txt', {encoding:'binary'},
+// (err, data)=>{
+//     err? console.log(err.code): console.log(`The file has \n ${data}`)
+// }
+// )
+try{
+    fs.writeFileSync('./testfolder/Synctest',data)
+    console.log('write operation succesful')
+    fileData = fs.readFileSync('./testfolder/Synctest', {encoding:'utf-16le'})
+    console.log(fileData)
+}catch(err){
+    console.log(err.code)
 
 }
-)
-fs.writeFile('./testfolder/test.txt', data,{flag:'a'},
-(err)=>{
-    err? console.log(err.code): console.log("Write succesfull")
-}
-)
-fs.readFile('./testfolder/test.txt', {encoding:'utf8'},
-(err, data)=>{
-    err? console.log(err.code): console.log(`The file has \n ${data}`)
-}
-)

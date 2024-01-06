88428c1bc7e0f72c1c9da1eafa5839085504ba97
tjay1760 <mcjthiongo@gmail.com>
2024-01-07 01:37:23 +0300

    Succesful create and read

--- fs.js
+++ fs.js
@@ -1,9 +1,14 @@
 const fs = require ('fs');
-
+const data = 'My first node redone again'
 
 fs.mkdir('./testfolder', 
 (err) => {
-    err ? console.log(err): console.log("Folder created succesfully") 
+    err ? console.log(err.code): console.log("Folder created succesfully") 
 
+}
+)
+fs.writeFile('./testfolder/test.txt', data,
+(err)=>{
+    err? console.log(err.code): console.log("Write succesfull")
 }
 )

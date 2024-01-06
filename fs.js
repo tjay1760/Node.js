a5e7842d0035e104ab50292e7bbfbfcbc96b8f72
tjay1760 <mcjthiongo@gmail.com>
2024-01-07 02:10:12 +0300

    read directory

--- fs.js
+++ fs.js
@@ -17,12 +17,15 @@ const data = '\n My first node redone again'
 //     err? console.log(err.code): console.log(`The file has \n ${data}`)
 // }
 // )
-try{
-    fs.writeFileSync('./testfolder/Synctest',data)
-    console.log('write operation succesful')
-    fileData = fs.readFileSync('./testfolder/Synctest', {encoding:'utf-16le'})
-    console.log(fileData)
-}catch(err){
-    console.log(err.code)
+// try{
+//     fs.writeFileSync('./testfolder/Synctest',data)
+//     console.log('write operation succesful')
+//     fileData = fs.readFileSync('./testfolder/Synctest', {encoding:'utf-16le'})
+//     console.log(fileData)
+// }catch(err){
+//     console.log(err.code)
 
-}
+// }
+fs.readdir('./testfolder',(err, data)=>{
+err? console.log(err.code): console.log(`Your files ${data}`)
+})

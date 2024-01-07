89e7a650b95a9999272129cff1ea7990a529c284
tjay1760 <mcjthiongo@gmail.com>
2024-01-07 10:39:49 +0300

    adding two events to emmit

--- events.js
+++ events.js
@@ -4,7 +4,10 @@ const myEmmiter = new eventEmmiter;
 const welcomeUser = (name) =>{
 console.log(`Welcome ${name} my good friend`)
 }
-
+const welcomeUser2 = () =>{
+    console.log(`Welcome tjay my good friend`)
+    }
 myEmmiter.on('welcomer', welcomeUser)
+myEmmiter.on('welcomer', welcomeUser2)
 
 myEmmiter.emit('welcomer')

37df5fdd75a8e6f383fa2933877d8082cc2a686e
tjay1760 <mcjthiongo@gmail.com>
2024-01-11 14:58:25 +0300

    refactor if else to add res.end

--- http.js
+++ http.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    req.url == '/'? res.write('<h1>Ny header request</h1>'):res.end('Not available')
+    if (req.url == '/'){
+         res.write('<h1>Ny header request</h1>')
+         res.end()
+    }else{
+        res.end('Not available')}
 })
 server.listen('5000',()=>{
     console.log('Server listening at port 5000')

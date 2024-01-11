76c6ebb7ced7ab9c5234d8a692ebe9996c5dca14
tjay1760 <mcjthiongo@gmail.com>
2024-01-11 15:29:07 +0300

    Succesfull port listen

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
+const http = require('http')
 
 const home = fs.readFileSync('./navbar/index.html')
+const server = http.createServer(
+    (req,res)=>{
+        res.writeHead(200,{'content/type':'text/html'})
+        res.write(home);
+        res.end();
+    }
+)
+server.listen(5000, ()=>{
+    console.log('Were listening to port 5000')
+})

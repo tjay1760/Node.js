2c1ef19db4261876d748d7be16c60ac05bfceed9
tjay1760 <mcjthiongo@gmail.com>
2024-01-11 14:49:53 +0300

    fix server listening to

--- http.js
+++ http.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    res.end('hello world')
+    req.url == '/'? res.end('hello world'):res.end('Not available')
 })
 server.listen('5000',()=>{
     console.log('Server listening at port 5000')

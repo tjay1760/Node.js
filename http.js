9f5bbd00200f915309460d4295a9528f62e194d7
tjay1760 <mcjthiongo@gmail.com>
2024-01-11 14:55:39 +0300

    result end with ternary

--- http.js
+++ http.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    req.url == '/'? res.end('hello world'):res.end('Not available')
+    req.url == '/'? res.write('<h1>Ny header request</h1>'):res.end('Not available')
 })
 server.listen('5000',()=>{
     console.log('Server listening at port 5000')

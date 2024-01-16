db46cfc5644cbd2b4a20def20c548ba3d64cb785
tjay1760 <mcjthiongo@gmail.com>
2024-01-17 00:12:41 +0300

    Module caching

--- Codevolution/main.js
+++ Codevolution/main.js
@@ -1,5 +1,10 @@
-const sum = require('./add')
-require ('./batman')
-require('./superman')
-console.log("Hello world from index.js")
-console.log(sum(2,4))
+const supeHero = require('./supeHero')
+const SuperHero = require ('./supeHero')
+console.log(SuperHero)
+console.log(SuperHero.name)
+SuperHero.setName('Superman')
+console.log(SuperHero.name)
+
+const newSuperHero = require('./supeHero')
+console.log(newSuperHero.name)
+

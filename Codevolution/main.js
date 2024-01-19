6255918f1bd94a6a6c79127368e4cffb0150d8e7
tjay1760 <mcjthiongo@gmail.com>
2024-01-19 21:07:55 +0300

    import json data

--- Codevolution/main.js
+++ Codevolution/main.js
@@ -1,6 +1,2 @@
-const math = require('./math')
-const {add,subtract} = math;
-resadd = add (1,2)
-ressub = subtract(1,2)
-console.log(resadd)
-console.log(ressub)
+const batmanData = require('./data.json')
+console.log(batmanData)

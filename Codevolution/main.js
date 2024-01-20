903f7747de23c9c25f4e226e191e1a110bef54f9
tjay1760 <mcjthiongo@gmail.com>
2024-01-20 15:06:04 +0300

    first emmiter

--- Codevolution/main.js
+++ Codevolution/main.js
@@ -1,9 +1,8 @@
-function greet (name){
-    console.log(`Hello there im ${name}`)
-}
-function greetme(greetFn){
-    const name = "Tjay"
-    greetFn(name)
-}
-
-greetme(greet)
+const eventEmmitter = require('events')
+const emmiter = new eventEmmitter()
+// listen to the emmiter
+emmiter.on("order-pizza", ()=>{
+    console.log("Emmiter started")
+})
+// broadcasting event
+emmiter.emit('order-pizza')

958e2f49f60adc7c9d7e45109923182d6ca4110e
tjay1760 <mcjthiongo@gmail.com>
2024-01-20 15:14:09 +0300

    multiple events

--- Codevolution/main.js
+++ Codevolution/main.js
@@ -4,5 +4,9 @@ const emmiter = new eventEmmitter()
 emmiter.on("order-pizza", (a,b)=>{
     console.log(`A ${a} and ${b} pizza ordered`)
 })
+emmiter.on("bake-pizza",(a,b)=>{
+    console.log(`Baking ${a} and ${b} pizza`)
+})
 // broadcasting event
 emmiter.emit('order-pizza', "large", "pinapple")
+// emmiter.emit('bake-pizza', "large", "pinapple")

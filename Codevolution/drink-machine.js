bff1ec8f2fcbc71e4b766e6cecb09918ffb4e8cf
tjay1760 <mcjthiongo@gmail.com>
2024-01-20 19:02:06 +0300

    Add drink machine class

---
+++ Codevolution/drink-machine.js
@@ -0,0 +1,7 @@
+class DrinkMAchine{
+    serveDrink(size){
+        size==="Large"?console.log("Large drink"): console.log("Small drink")
+
+    }
+}
+module.exports = DrinkMAchine
--- Codevolution/main.js
+++ Codevolution/main.js
@@ -1,7 +1,10 @@
 const pizzaShop = require('./pizzaShop')
+const DrinkMachine = require('./drink-machine')
 mypizzashop = new pizzaShop();
+drink_machine = new DrinkMachine();
 mypizzashop.on("order", (size,topping)=>{
     console.log(`Your pizza is ${size} with ${topping} toppings `)
+    drink_machine.serveDrink(size)
 })
 mypizzashop.order("Large", "Pinapple")
 mypizzashop.display() 

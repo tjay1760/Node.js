87549de717ae997eb505b9fdf66f7b264025a779
tjay1760 <mcjthiongo@gmail.com>
2024-01-20 15:10:26 +0300

    Passing params

--- Codevolution/main.js
+++ Codevolution/main.js
@@ -1,8 +1,8 @@
 const eventEmmitter = require('events')
 const emmiter = new eventEmmitter()
 // listen to the emmiter
-emmiter.on("order-pizza", ()=>{
-    console.log("Emmiter started")
+emmiter.on("order-pizza", (a,b)=>{
+    console.log(`A ${a} and ${b} pizza ordered`)
 })
 // broadcasting event
-emmiter.emit('order-pizza')
+emmiter.emit('order-pizza', "large", "pinapple")

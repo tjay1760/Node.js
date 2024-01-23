addc369331c3c194b4c207a648670898bf9c0c36
tjay1760 <mcjthiongo@gmail.com>
2024-01-24 02:39:19 +0300

    json response

--- Codevolution/add.js
+++ Codevolution/add.js
@@ -1,5 +1,6 @@
 const add = (a,b) =>{
     return a+b
     }
-
+res = [1,2,3,4,5,6][1,4]
+console.log(res)
 module.exports = add;
--- Codevolution/http.js
+++ Codevolution/http.js
@@ -1,8 +1,13 @@
 const http = require('http')
 const server = http.createServer(
     (req,res)=>{
-res.writeHead(200, {"Content-Type":"text/plain"})
-res.end("HEllo World")
+const superHero = {
+    firstname:"Bruce",
+    secondName:"Wayne"
+}
+payload = JSON.stringify(superHero)
+res.writeHead(200, {"Content-Type":"application/json"})
+res.end(payload)
     }
 )
 server.listen(5000,()=>{

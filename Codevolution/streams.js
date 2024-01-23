fd07f81a4f49affbc86c74c670a5f31fd559cb11
tjay1760 <mcjthiongo@gmail.com>
2024-01-23 15:49:09 +0300

    Chaining with gZip

--- Codevolution/streams.js
+++ Codevolution/streams.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
+const zlib = require("zlib")
+const gzip = zlib.createGzip()
 const readableStream = fs.createReadStream("./streams.txt",{encoding:"utf8",highWaterMark:2})
+readableStream.pipe(gzip).pipe(fs.WriteStream('./file.txt.gz'))
 const writablestream = fs.createWriteStream("./streamswrite.txt",{encoding:"utf-8"})
 // readableStream.on("data",(chunk)=>{
 // console.log(chunk)

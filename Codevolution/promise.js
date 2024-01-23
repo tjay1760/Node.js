217644a7d97cc10f2574b880d9aacba5b19b47b6
tjay1760 <mcjthiongo@gmail.com>
2024-01-23 13:53:15 +0300

    async await version

--- Codevolution/promise.js
+++ Codevolution/promise.js
@@ -1,6 +1,18 @@
 const fs = require("fs/promises");
-
+console.log("before read")
 fs.readFile("./welcome.txt","utf-8",)
 .then(
     data=>{console.log(data)}
 ).catch(err=>{console.log(err)})
+console.log("after read")
+// async await version
+async function readFile() {
+    try{
+const data = await fs.readFile("./welcome.txt","utf-8")
+console.log(data)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+readFile()

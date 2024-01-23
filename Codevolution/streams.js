0745e20d086290486b30afc5c24aba3fe0762de9
tjay1760 <mcjthiongo@gmail.com>
2024-01-23 14:53:10 +0300

    working with streams

--- Codevolution/streams.js
+++ Codevolution/streams.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const readableStream = fs.createReadStream("./streams.txt",{encoding:"utf8"})
+const readableStream = fs.createReadStream("./streams.txt",{encoding:"utf8",highWaterMark:2})
 const writablestream = fs.createWriteStream("./streamswrite.txt",{encoding:"utf-8"})
 readableStream.on("data",(chunk)=>{
 console.log(chunk)

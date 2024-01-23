7f2103202eb34fde4ff546379a36e90e11809af9
tjay1760 <mcjthiongo@gmail.com>
2024-01-23 12:51:15 +0300

    Async data read

--- Codevolution/fs.js
+++ Codevolution/fs.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 const content = fs.readFileSync("./file.txt", "utf-8")
 console.log(content)
+fs.readFile("./file.txt","utf-8",(err,res)=>{
+err?console.log(err): console.log(res)
+})

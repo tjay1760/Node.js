61295f66b732f47829a37649c7f4b388898605b9
tjay1760 <mcjthiongo@gmail.com>
2024-01-23 12:53:52 +0300

    async check

--- Codevolution/fs.js
+++ Codevolution/fs.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const content = fs.readFileSync("./file.txt", "utf-8")
+console.log("Synchrounous")
 console.log(content)
+console.log("Async read")
 fs.readFile("./file.txt","utf-8",(err,res)=>{
 err?console.log(err): console.log(res)
 })
+console.log("program end")

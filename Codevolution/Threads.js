31333ae976279f6d7a0e634f2692e9b5d0aff5a0
tjay1760 <mcjthiongo@gmail.com>
2024-01-26 23:08:38 +0300

    How the event loop works

--- Codevolution/Threads.js
+++ Codevolution/Threads.js
@@ -1,11 +1,18 @@
-const crypto = require("crypto")
+// const crypto = require("crypto")
+const https = require('https')
 const start = Date.now();
-process.env.UV_THREADPOOL_SIZE = 2;
-const MAX_CALLS = 2;
+// process.env.UV_THREADPOOL_SIZE = 2;
+const MAX_CALLS = 10;
 for (let i=0; i<MAX_CALLS;i++){
-    crypto.pbkdf2("password","salt",100000,512,"sha512",()=>{
-        console.log(`Hash: ${i+1}`, Date.now()-start)
-    })
+    https.request("https://www.google.com",(res)=>{
+        res.on("data",()=>{})
+        res.on("end",()=>{
+            console.log(`Request ${i+1}`, Date.now()- start)
+        })
+    }).end();
+    // crypto.pbkdf2("password","salt",100000,512,"sha512",()=>{
+    //     console.log(`Hash: ${i+1}`, Date.now()-start)
+    // })
 }
 
 

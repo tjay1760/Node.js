dde569e5c01172f781996910e3e4a3676f630805
tjay1760 <mcjthiongo@gmail.com>
2024-01-26 13:47:09 +0300

    thread pool

--- Codevolution/Threads.js
+++ Codevolution/Threads.js
@@ -1,5 +1,15 @@
 const crypto = require("crypto")
 const start = Date.now()
+const MAX_CALLS = 3;
+// for (let i=0; i<MAX_CALLS;i++){
+//     crypto.pbkdf2("password","salt",100000,512,"sha512",()=>{
+//         console.log(`Hash: ${i+1}`, Date.now()-start)
+//     })
+// }
+
+
 crypto.pbkdf2Sync("password","salt",100000,512,"sha512")
 crypto.pbkdf2Sync("password","salt",100000,512,"sha512")
+crypto.pbkdf2Sync("password","salt",100000,512,"sha512")
+
 console.log("Hash:", Date.now()-start)

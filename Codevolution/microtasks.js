f917d667f6f9a2f9e48d703089af130b042e3334
tjay1760 <mcjthiongo@gmail.com>
2024-01-26 23:43:34 +0300

    promises and nextTicks

--- Codevolution/microtasks.js
+++ Codevolution/microtasks.js
@@ -1,5 +1,12 @@
-console.log("ONE")
+// console.log("ONE")
+// process.nextTick(()=>{
+//     console.log("Process.nextick 1")
+// })
+// console.log("TWO")
+
+Promise.resolve().then(()=>{
+    console.log("Promise.resolve 1")
+})
 process.nextTick(()=>{
     console.log("Process.nextick 1")
 })
-console.log("TWO")

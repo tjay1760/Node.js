b3d519020c1f8d4f96a357200ffd0627fcdc6d76
tjay1760 <mcjthiongo@gmail.com>
2024-01-29 21:17:58 +0300

    hit that API

--- Codevolution/custom_CLI/index.js
+++ Codevolution/custom_CLI/index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
 console.log("Wow Yo made your CLI")
+const fetchPokeMoves = async (pokemon)=>{
+const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+const data = await response.json()
+const abilities = data.abilities.map((ability)=>
+    ability.ability.name
+)
+console.log(abilities)
+}
+fetchPokeMoves("8")

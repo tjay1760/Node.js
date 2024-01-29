c6182b29c17242efe54ab3d241b3b9e3078d33f3
tjay1760 <mcjthiongo@gmail.com>
2024-01-29 21:59:22 +0300

    add interactivity

--- Codevolution/custom_CLI/index.js
+++ Codevolution/custom_CLI/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const inquirer = require("inquirer")
 console.log("Wow Yo made your CLI")
 const fetchPokeMoves = async (pokemon)=>{
 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -8,4 +9,13 @@ const abilities = data.abilities.map((ability)=>
 )
 console.log(abilities)
 }
-fetchPokeMoves("8")
+const prompt = inquirer.createPromptModule()
+prompt([{
+    type:"input",
+    name:"pokemon",
+    message:"Enter pokemon value"
+}]).then((answers)=>{
+const pokemon = answers.pokemon
+console.log("PPokemon val",pokemon)
+fetchPokeMoves(pokemon)
+})

a963fba4e75e239737183aaafb6b06ffdf347af5
tjay1760 <mcjthiongo@gmail.com>
2024-02-01 18:27:27 +0300

    passing params to id

--- CodewithMosh/express/index.js
+++ CodewithMosh/express/index.js
@@ -1,1 +1,17 @@
- const 
+ const express = require('express');
+ const app = express();
+ app.get('/',(req,res)=>{
+    res.send("Hi there")
+ })
+ app.get('/api/v1',(req,res)=>{
+    res.send([1,2,3])
+ })
+
+app.get("/api/v1/:id",(req,res)=>{
+    res.send(req.params)
+})
+ //PORT
+ port = process.env.PORT || 3000;
+ app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+ })

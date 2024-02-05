5076f1cd5742ff61ce0494854fdca6bd4b124a8f
tjay1760 <mcjthiongo@gmail.com>
2024-02-05 03:49:50 +0300

    using callbacks

--- CodewithMosh/async/index.js
+++ CodewithMosh/async/index.js
@@ -1,11 +1,23 @@
-const getUser = ()=>{
-    setTimeout((id)=>{
+const getUser = (id, callback)=>{
+    setTimeout(()=>{
         console.log("Reading user from DB")
-        return {id:id, username:"tjay"}
+        callback({id:id, username:"tjay"})
     })
 }
+const getRepos = (username, callback)=>{
+    setTimeout(()=>{
+        callback(['repo1','repo2','repo3'])
+    },2000)
+
+}
 console.log("Before");
-const user = getUser(5)
-console.log(user)
+getUser(5,(user)=>{
+console.log(`The user ${user.id} is ${user.username}`)
+})
+getRepos("tjay",(repos)=>{
+repos.forEach(repo => {
+    console.log(`This is a ${repo}`)
+});
+})
 console.log("After");
 

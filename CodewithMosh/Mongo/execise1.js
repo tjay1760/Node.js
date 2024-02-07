91c6577638b7505a175297163e3da85cd22d8ad2
tjay1760 <mcjthiongo@gmail.com>
2024-02-07 06:49:25 +0300

    exercise complete

--- CodewithMosh/Mongo/execise1.js
+++ CodewithMosh/Mongo/execise1.js
@@ -2,3 +2,21 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongo-exercises')
 .then(()=>console.log("Database connection succesful"))
 .catch(err=>console.log(err))
+
+
+const courseSchema = new mongoose.Schema({
+    name:"string",
+    author:"string",
+    tags:[String],
+    date: {type:Date, default: Date.now()},
+    isPublished: Boolean,
+    price: Number
+});
+const Course = mongoose.model('Course',courseSchema);
+
+async function getCourses(){
+    console.log("gettiing courses")
+    const courses = await Course.find().sort({name:1, author:1}).select({name:1, author:1})
+    console.log(courses)
+}
+getCourses()

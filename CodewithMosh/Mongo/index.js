3e94a7fd2dbcb27c9b99b3f845ae9c28130efce2
tjay1760 <mcjthiongo@gmail.com>
2024-02-07 04:16:20 +0300

    quering objects

---
+++ CodewithMosh/Mongo/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/playground')
+.then(()=>console.log("Connected to MMongodb Succesfully")).catch(err=>(console,log(`FAiled to connect:: ${err}`)))
+const courseSchema = new mongoose.Schema({
+    name:"string",
+    author:"string",
+    tags:[String],
+    date: {type:Date, default: Date.now()},
+    isPublished: Boolean
+});
+const Course = mongoose.model('Course',courseSchema);
+
+async function createCourse(){
+    const course = new Course({
+        name:"REact",
+        author:"Theo",
+        tags:["front-end","javascript","DOM"],
+        isPublished: true
+    });
+    const result = await course.save();
+    console.log(result)
+}
+async function getCourses(){
+const courses = await Course.find({author:"Theo"}).select({name:1,tags:1});
+console.log(courses)
+}
+getCourses();
+// createCourse();
